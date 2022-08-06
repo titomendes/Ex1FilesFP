7526a0170486b695943d17814b983abcf495f08a
Tito <titodalt@gmail.com>
2022-08-06 07:23:26 +0100

    Versao antes de tentar tirar os \n

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #include <stdbool.h>
 
 
-const char *formatOut="%s %d %s %d %d\n"; //formato para ser escrito no ficheiro
+const char *formatOut="%s,%d,%s,%d,%d\n"; //formato para ser escrito no ficheiro
 
 typedef struct{
     int num;
-    char nome[50];
-    char curso[20];
+    char nome[80];
+    char curso[80];
     int nota1;
     int nota2;
 
@@ -22,17 +22,16 @@ void mudaNota();
 void sairJanela();
 void menuClose();
 
-int main(){
-
+int main()
+{
  Alunos *aluno=(Alunos*)malloc(sizeof(aluno));
  menu(aluno);
-
-
 
     return 0;
 }
 
-void menu(Alunos *aluno){
+void menu(Alunos *aluno)
+{
     printf("Inserir aluno: opcao 1");
     printf("\nMudar de nota: opcao 2");
     printf("\nApagar contas: opcao 3\n");
@@ -58,24 +57,45 @@ void menu(Alunos *aluno){
 void mudaNota(Alunos *aluno)
 {
    FILE *p;
+   int numAluno;
    p= fopen("Alunos.dat","r+");//r+ leitura e escrita
    if(p==NULL)
       {
         perror("Erro, arquivo nao aberto");
       }
-   printf("Nome completo do aluno a mudar notas: ");
-
+   printf("Numero do aluno a mudar detalhes: ");
+   scanf("%d",&numAluno);
+   //começar aqui usar strtok para chegar a linha onde esta o nome e fazer break.imprimir a linha para ver se esta na linha correcta
 }
 
 bool findNumber(int number)
 {
     FILE *p;
+    char temp[50];
+    char d[]=",";
+    bool flag=false;
+
     p= fopen("Alunos.dat","r");
      if(p==NULL)
       {
         perror("Erro, arquivo nao aberto");
       }
-
+     while(!(feof(p)))
+     {
+        fgets(temp,50,p);
+        char *portion= strtok(temp,d);
+        while(portion!=NULL)
+        {
+           if(atoi(portion)==number)
+           {
+              flag=true;
+              break;
+           }
+          portion=strtok(NULL,d);
+        }
+    }
+    fclose(p);
+    return flag;
 }
 
 void inserirAlunos(Alunos *aluno)
@@ -89,14 +109,20 @@ void inserirAlunos(Alunos *aluno)
         getchar();
         printf("Numero do aluno: ");
         scanf("%d",&aluno->num);
-       /* while(findNumber(aluno->num)==true)
+        while(findNumber(aluno->num)==true)
         {
             printf("Numero ja existe,insira outro: ");
             scanf("%d",&aluno->num);
-        }*/
+        }
         getchar();
         printf("Nome do aluno:");
         scanf("%s",aluno->nome);
+        for(int i=0;i<strlen(aluno->nome);i++){
+            if((aluno->nome[i])=='.')
+            {
+                aluno->nome[i]=' ';
+            }
+        }
         printf("Nota1 do aluno: ");
         scanf("%d",&aluno->nota1);
         printf("Nota2 do aluno: ");
@@ -104,6 +130,13 @@ void inserirAlunos(Alunos *aluno)
         getchar();
         printf("Curso do aluno: ");
         scanf("%s",aluno->curso);
+        for(int i=0;i<strlen(aluno->curso);i++){
+            if((aluno->curso[i])=='.')
+            {
+                aluno->curso[i]=' ';
+            }
+        }
+        fflush(stdin);
         fprintf(p,formatOut,aluno->nome,aluno->num,aluno->curso,aluno->nota1,aluno->nota2);
         fclose(p);
         free(aluno);
